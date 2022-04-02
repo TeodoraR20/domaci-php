baf46209e05a15653a84f885f8a6c57382e00990
Teodora Radovic <tradovic19@gmail.com>
2022-04-02 23:48:02 +0200

    Dodata pocetna strana

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+//require dbBroker.php
+
+//require "model/prijava.php"
+
+/*
+session_start();
+if(!isset($_SESSION['user_id'])){
+
+header('Location:index.php');
+exit();
+}
+*/
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="css/style.css" rel="stylesheet">
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+
+    <?php
+    include('head.php');
+    ?>
+
+<style>
+
+body {
+  background-image: url("img/rent.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 500px;
+}
+
+</style>
+
+    <title>Iteh aplikacija</title>
+</head>
+<body>
+
+
+<!--
+
+<div class="p-5 bg-primary text-center text-color:white">
+  <h1>Rent a bike</h1>
+  
+</div>
+-->
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
